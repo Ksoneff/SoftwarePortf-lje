b2e7d016807cb10f5a1a92247dec40c76ccaabef
Karl Soneff <karl@soneff.com>
2025-04-23 20:00:10 +0200

    New monster (Unicorn) class added

---
+++ Unicorn.cpp
@@ -0,0 +1,5 @@
+#include "Unicorn.h"
+
+Unicorn::Unicorn() : Monster("Unicorn", 5, 8, 1500) {}
+
+Unicorn::~Unicorn() {}
---
+++ Unicorn.h
@@ -0,0 +1,12 @@
+#ifndef UNICORN_H
+#define UNICORN_H
+
+#include "Monster.h"
+
+class Unicorn : public Monster {
+public:
+	Unicorn();
+	~Unicorn();
+};
+
+#endif
