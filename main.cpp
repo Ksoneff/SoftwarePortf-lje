cf559d2babf1b4a069ca6949b64fcd7ae26c4d7f
Karl Soneff <karl@soneff.com>
2025-04-23 20:04:16 +0200

    Main program file has been added

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "Hero.h"
+#include "Controller.h"
+#include "Monster.h"
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	Controller game("New Game");
+
+}
