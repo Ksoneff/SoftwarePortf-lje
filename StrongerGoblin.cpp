d6dc5635dd8136e5ad0cc1feb9f7572b16b922aa
Karl Soneff <karl@soneff.com>
2025-04-23 19:55:38 +0200

    New monster (Stronger Goblin) class added

---
+++ StrongerGoblin.cpp
@@ -0,0 +1,5 @@
+#include "StrongerGoblin.h"
+
+StrongerGoblin::StrongerGoblin() : Monster("Stronger Goblin", 10, 4, 500) {}
+
+StrongerGoblin::~StrongerGoblin() {}
---
+++ StrongerGoblin.h
@@ -0,0 +1,12 @@
+#ifndef STRONGERGOBLIN_H
+#define STRONGERGOBLIN_H
+
+#include "Monster.h"
+
+class StrongerGoblin : public Monster {
+public:
+	StrongerGoblin();
+	~StrongerGoblin();
+};
+
+#endif
