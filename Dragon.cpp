49d8a19efb1478bee24243e0c4b1e2932560addb
Karl Soneff <karl@soneff.com>
2025-04-23 20:01:27 +0200

    New monster (Dragon) class added

---
+++ Dragon.cpp
@@ -0,0 +1,5 @@
+#include "Dragon.h"
+
+Dragon::Dragon() : Monster("Dragon", 100, 10, 3000) {}
+
+Dragon::~Dragon() {}
---
+++ Dragon.h
@@ -0,0 +1,12 @@
+#ifndef DRAGON_H
+#define DRAGON_H
+
+#include "Monster.h"
+
+class Dragon : public Monster {
+public:
+	Dragon();
+	~Dragon();
+};
+
+#endif
