2eca05e609de1b833337cb08a9ed3dd60a6781c6
Karl Soneff <karl@soneff.com>
2025-04-23 19:57:12 +0200

    New monster (The Strongest Goblin) class added

---
+++ TheStrongestGoblin.cpp
@@ -0,0 +1,5 @@
+#include "TheStrongestgoblin.h"
+
+TheStrongestGoblin::TheStrongestGoblin() : Monster("The Strongest Goblin", 15, 5, 800) {}
+
+TheStrongestGoblin::~TheStrongestGoblin() {}
---
+++ TheStrongestGoblin.h
@@ -0,0 +1,12 @@
+#ifndef THESTRONGESTGOBLIN_H
+#define THESTRONGESTGOBLIN_H
+
+#include "Monster.h"
+
+class TheStrongestGoblin : public Monster {
+public:
+    TheStrongestGoblin();
+    ~TheStrongestGoblin();
+};
+
+#endif
