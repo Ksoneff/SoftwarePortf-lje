86fc41a1a3b1638f6599af18580066d84490aa83
Karl Soneff <karl@soneff.com>
2025-04-23 19:58:31 +0200

    New monster (Ape King) class added

---
+++ ApeKing.cpp
@@ -0,0 +1,5 @@
+#include "ApeKing.h"
+
+ApeKing::ApeKing() : Monster("Ape King", 30, 5, 1000) {}
+
+ApeKing::~ApeKing() {}
---
+++ ApeKing.h
@@ -0,0 +1,12 @@
+#ifndef APEKING_H
+#define APEKING_H
+
+#include "Monster.h"
+
+class ApeKing : public Monster {
+public:
+	ApeKing();
+	~ApeKing();
+};
+
+#endif
