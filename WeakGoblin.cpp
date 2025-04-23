f09d0d2f281a8794bd51946baa6fa2fd9aba35d0
Karl Soneff <karl@soneff.com>
2025-04-23 19:52:40 +0200

    New monster (Weak Goblin) class added

---
+++ WeakGoblin.cpp
@@ -0,0 +1,5 @@
+#include "WeakGoblin.h"
+
+WeakGoblin::WeakGoblin() : Monster("Weak Goblin", 4, 2, 200) {}
+
+WeakGoblin::~WeakGoblin() {}
---
+++ WeakGoblin.h
@@ -0,0 +1,12 @@
+#ifndef WEAKGOBLIN_H
+#define WEAKGOBLIN_H
+
+#include "Monster.h"
+
+class WeakGoblin : public Monster {
+public:
+    WeakGoblin();
+    ~WeakGoblin();
+};
+
+#endif
